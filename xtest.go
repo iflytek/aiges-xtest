123fbc75a87429756ff809b1a739e9fa8bef966d
yqwu18 <yqwu18@iflytek.com>
2022-10-17 13:12:08 +0800

    update nvml shutdown
    Signed-off-by: yqwu18 <yqwu18@iflytek.com>

--- xtest.go
+++ xtest.go
@@ -58,10 +58,12 @@ func (x *Xtest) Run() {
 	}
 
 	err := nvml.Init()
-	defer nvml.Shutdown()
 	if err != nil {
 		log.Printf("can't get nvml lib..\n %s", err.Error())
 		x.r.C.GpuMon = false
+	}else{
+	
+		defer nvml.Shutdown()
 	}
 	// 启动一个系统资源定时任务
 	stp.Start(time.Microsecond*100, func() {
